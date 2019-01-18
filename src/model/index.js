622199ee5d5bf90bad0e54f52450bd6740c2d70b
Vincent Laude <vincent.laude2@gmail.com>
2019-01-18 22:40:36 +0100

    Remove attempt connection to sequelize

--- src/model/index.js
+++ src/model/index.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const logger = require('../logger');
 
 const db = {};
 const basename = path.basename(module.filename);
@@ -28,14 +27,5 @@ Object.keys(db).forEach(modelName => {
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
-sequelize
-  .authenticate()
-  .then(() => {
-    logger.info('Connection has been established successfully.');
-  })
-  .catch(err => {
-    logger.error('Unable to connect to the database:', err);
-  });
 
 module.exports = db;
