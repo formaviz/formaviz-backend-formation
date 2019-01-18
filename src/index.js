e23896c1d72c093e990a960a4eea3a01c78715f4
Vincent Laude <vincent.laude2@gmail.com>
2019-01-18 22:41:16 +0100

    Add api launcher with sequelize connection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-const db = require('./model/index');
+const api = require('./api');
+const db = require('./model');
+const logger = require('./logger');
+
+const port = process.env.PORT || 3000;
+const ip = process.env.IP | 'localhost';
+
+db.sequelize
+  .sync()
+  .then(() =>
+    api.listen(port, ip, err =>
+      err
+        ? logger.error(`🔥 Failed to start API : ${err.stack}`)
+        : logger.info(`🌎 API is listening on port ${port}`)
+    )
+  )
+  .catch(err => logger.error(`🔥 Failed to connect database : ${err.stack}`));
