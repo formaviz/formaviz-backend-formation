9ec2aca7d07dd22abd22d407c0e04517df7a80ac
Vincent Laude <vincent.laude2@gmail.com>
2019-01-29 16:42:51 +0100

    WIP deploy on clevercloud

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const api = require('./api/index');
 const db = require('./model');
 const logger = require('./logger').logger;
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const ip = process.env.IP || '0.0.0.0';
 
 db.sequelize
