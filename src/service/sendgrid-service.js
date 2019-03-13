4dbc45a40226be96f9646b0730c55aa0ddbbe533
toche <christophe.maingard@epitech.eu>
2019-03-13 20:19:07 +0100

    feat(sendgrid): add no-reply

--- src/service/sendgrid-service.js
+++ src/service/sendgrid-service.js
@@ -3,12 +3,14 @@
 const sgMail = require('@sendgrid/mail');
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const supportEmail = process.env.SUPPORT_EMAIL;
+const noReply = process.env.NO_REPLY_EMAIL;
 
-const sendEmail = (to, bcc, from, subject, text, html) => {
+const sendEmail = (to, bcc, subject, text, html) => {
   const msg = {
     to: `${to}`,
     bcc: Array.isArray(bcc) ? bcc : '',
-    from: `${from}`,
+    from: `${noReply}`,
     subject: `${subject}`,
     text: `${text}`,
     html: `${html}`,
@@ -16,6 +18,15 @@ const sendEmail = (to, bcc, from, subject, text, html) => {
   sgMail.send(msg);
 };
 
+const reportToSupport = (involvedRoute) => {
+  sendEmail(supportEmail,
+    null,
+    'Error on server',
+    `unable to contact ${involvedRoute}, please see logs to correct the problem.`,
+    `<span>unable to contact <strong>${involvedRoute}</strong>, please see logs to correct the problem.</span>`);
+};
+
 module.exports = {
   sendEmail,
+  reportToSupport
 };
